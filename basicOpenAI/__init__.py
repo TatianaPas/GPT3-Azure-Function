6134a1e54455a425259581d0651d4fe12f8ed345
Tatiana <tatjana.pasechnik@gmail.com>
2023-04-19 22:26:52 +1200

    first commit

---
+++ basicOpenAI/__init__.py
@@ -0,0 +1,25 @@
+import logging
+import openai
+import azure.functions as func
+
+secret_key = ''
+
+# sample request
+# {"model":"text-davinci-003", "prompt":"give the slogan to customised books company", "max_tokens":200, "temperature":0}
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    openai.api_key = secret_key
+
+    req_body = req.get_json()
+    output = openai.Completion.create(
+        model=req_body['model'],
+        prompt=req_body['prompt'],
+        max_tokens=req_body['max_tokens'],
+        temperature=req_body['temperature']
+    )
+
+    output_text = output['choices'][0]['text']
+
+    return func.HttpResponse(output_text, status_code=200)
